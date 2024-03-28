a6bbef13346ddbd09225b44b1c4af906a7700568
Artem Sitko <sitko.ar@gmail.com>
2024-03-28 20:34:00 +0300

    Add controller layer

---
+++ cab-app-passenger-service/src/main/java/by/arvisit/cabapp/passengerservice/controller/PassengerController.java
@@ -0,0 +1,83 @@
+package by.arvisit.cabapp.passengerservice.controller;
+
+import java.util.List;
+
+import org.hibernate.validator.constraints.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import by.arvisit.cabapp.passengerservice.dto.PassengerRequestDto;
+import by.arvisit.cabapp.passengerservice.dto.PassengerResponseDto;
+import by.arvisit.cabapp.passengerservice.service.PassengerService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/v1/passengers")
+@RequiredArgsConstructor
+@Validated
+@Slf4j
+public class PassengerController {
+
+    private final PassengerService passengerService;
+
+    @PostMapping
+    public ResponseEntity<PassengerResponseDto> save(@RequestBody @Valid PassengerRequestDto dto) {
+        PassengerResponseDto response = passengerService.save(dto);
+
+        log.debug("New passenger was added: {}", response);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public PassengerResponseDto update(@PathVariable @UUID String id, @RequestBody @Valid PassengerRequestDto dto) {
+        PassengerResponseDto response = passengerService.update(id, dto);
+
+        log.debug("Passenger with id {} was updated and now is: {}", id, response);
+        return response;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable @UUID String id) {
+        passengerService.delete(id);
+
+        log.debug("Passenger with id {} was removed", id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/{id}")
+    public PassengerResponseDto getPassengerById(@PathVariable @UUID String id) {
+        PassengerResponseDto response = passengerService.getPassengerById(id);
+
+        log.debug("Got passenger by id {}: {}", id, response);
+        return response;
+    }
+
+    @GetMapping("/by-email/{email}")
+    public PassengerResponseDto getPassengerByEmail(@PathVariable @Email String email) {
+        PassengerResponseDto response = passengerService.getPassengerByEmail(email);
+
+        log.debug("Got passenger by email {}: {}", email, response);
+        return response;
+    }
+
+    @GetMapping
+    public List<PassengerResponseDto> getPassengers() {
+        List<PassengerResponseDto> response = passengerService.getPassengers();
+
+        log.debug("Got all passengers. Total count: {}", response.size());
+        return response;
+    }
+
+}
