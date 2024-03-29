4829c5b2e857ed6e85b72e3fe72694b784215a03
Artem Sitko <sitko.ar@gmail.com>
2024-03-29 14:15:15 +0300

    Apply new line formatting for record fields

--- cab-app-passenger-service/src/main/java/by/arvisit/cabapp/passengerservice/dto/PassengerResponseDto.java
+++ cab-app-passenger-service/src/main/java/by/arvisit/cabapp/passengerservice/dto/PassengerResponseDto.java
@@ -3,6 +3,10 @@ package by.arvisit.cabapp.passengerservice.dto;
 import lombok.Builder;
 
 @Builder(setterPrefix = "with")
-public record PassengerResponseDto(String id, String name, String email, String cardNumber) {
+public record PassengerResponseDto(
+        String id,
+        String name,
+        String email,
+        String cardNumber) {
 
 }
