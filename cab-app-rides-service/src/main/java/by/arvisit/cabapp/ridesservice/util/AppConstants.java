ac0a61ff95a3f20d50f3fd59d7ed804d613257a3
Artem Sitko <sitko.ar@gmail.com>
2024-04-08 09:25:05 +0300

    Add constants util

---
+++ cab-app-rides-service/src/main/java/by/arvisit/cabapp/ridesservice/util/AppConstants.java
@@ -0,0 +1,12 @@
+package by.arvisit.cabapp.ridesservice.util;
+
+import java.time.ZoneId;
+
+public final class AppConstants {
+
+    public static final int MAX_SCORE = 5;
+    public static final ZoneId EUROPE_MINSK_TIMEZONE = ZoneId.of("Europe/Minsk");
+    
+    private AppConstants() {
+    }
+}
