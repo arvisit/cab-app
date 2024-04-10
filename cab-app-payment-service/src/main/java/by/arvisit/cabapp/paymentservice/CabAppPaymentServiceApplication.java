f6f488c0b4528e5b7c2c2099e58cedfdd809c70f
Artem Sitko <sitko.ar@gmail.com>
2024-04-10 11:20:01 +0300

    Initialize payment service

---
+++ cab-app-payment-service/src/main/java/by/arvisit/cabapp/paymentservice/CabAppPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package by.arvisit.cabapp.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabAppPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabAppPaymentServiceApplication.class, args);
+    }
+
+}
