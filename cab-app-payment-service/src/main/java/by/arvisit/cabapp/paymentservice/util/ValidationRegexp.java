56ca0952e81c04dd6c3e60c6e6baeab8150746c9
Artem Sitko <sitko.ar@gmail.com>
2024-04-12 00:15:53 +0300

    Add utils

---
+++ cab-app-payment-service/src/main/java/by/arvisit/cabapp/paymentservice/util/ValidationRegexp.java
@@ -0,0 +1,9 @@
+package by.arvisit.cabapp.paymentservice.util;
+
+public final class ValidationRegexp {
+
+    public static final String CARD_NUMBER = "[0-9]{16}";
+
+    private ValidationRegexp() {
+    }
+}
