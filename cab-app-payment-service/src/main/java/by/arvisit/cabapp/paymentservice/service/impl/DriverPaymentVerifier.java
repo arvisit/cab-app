eca0b8ec70e07cf9a6850ea6dc1d2d50c49565f7
Artem Sitko <sitko.ar@gmail.com>
2024-04-16 01:07:00 +0300

    Add driver and passenger verifications for payments

--- cab-app-payment-service/src/main/java/by/arvisit/cabapp/paymentservice/service/impl/DriverPaymentVerifier.java
+++ cab-app-payment-service/src/main/java/by/arvisit/cabapp/paymentservice/service/impl/DriverPaymentVerifier.java
@@ -5,6 +5,8 @@ import java.math.BigDecimal;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
+import by.arvisit.cabapp.common.dto.driver.DriverResponseDto;
+import by.arvisit.cabapp.paymentservice.client.DriverClient;
 import by.arvisit.cabapp.paymentservice.persistence.model.DriverPayment;
 import by.arvisit.cabapp.paymentservice.persistence.model.OperationTypeEnum;
 import by.arvisit.cabapp.paymentservice.persistence.repository.DriverPaymentRepository;
@@ -15,11 +17,14 @@ import lombok.RequiredArgsConstructor;
 class DriverPaymentVerifier {
 
     private static final String LOW_BALANCE_MESSAGE_TEMPLATE_KEY = "by.arvisit.cabapp.paymentservice.persistence.model.DriverPayment.balance.IllegalStateException.template";
+    private static final String CARD_NUMBER_MISMATCH_MESSAGE_TEMPLATE_KEY = "by.arvisit.cabapp.paymentservice.persistence.model.DriverPayment.cardNumber.IllegalStateException.template";
 
     private final DriverPaymentRepository driverPaymentRepository;
     private final MessageSource messageSource;
+    private final DriverClient driverClient;
 
     public void verifyNewPayment(DriverPayment newPayment) {
+        verifyDriverForNewPayment(newPayment);
         verifyBalanceForNewPayment(newPayment);
     }
 
@@ -32,6 +37,18 @@ class DriverPaymentVerifier {
 
             String errorMessage = messageSource.getMessage(
                     LOW_BALANCE_MESSAGE_TEMPLATE_KEY,
+                    new Object[] {}, null);
+            throw new IllegalStateException(errorMessage);
+        }
+    }
+
+    public void verifyDriverForNewPayment(DriverPayment newPayment) {
+        DriverResponseDto driver = driverClient.getDriverById(newPayment.getDriverId().toString());
+
+        if (!newPayment.getCardNumber().equals(driver.cardNumber())) {
+
+            String errorMessage = messageSource.getMessage(
+                    CARD_NUMBER_MISMATCH_MESSAGE_TEMPLATE_KEY,
                     new Object[] {}, null);
             throw new IllegalStateException(errorMessage);
         }
