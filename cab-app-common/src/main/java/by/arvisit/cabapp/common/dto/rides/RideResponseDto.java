51e15b30dc67147e5582bd7837d880926d442593
Artem Sitko <sitko.ar@gmail.com>
2024-04-16 12:13:37 +0300

    Add ride response dto to common lib

---
+++ cab-app-common/src/main/java/by/arvisit/cabapp/common/dto/rides/RideResponseDto.java
@@ -0,0 +1,30 @@
+package by.arvisit.cabapp.common.dto.rides;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+import lombok.Builder;
+
+@Builder(setterPrefix = "with")
+public record RideResponseDto(
+        String id,
+        BigDecimal initialCost,
+        BigDecimal finalCost,
+        String passengerId,
+        String driverId,
+        String promoCode,
+        String status,
+        String paymentMethod,
+        Boolean isPaid,
+        String startAddress,
+        String destinationAddress,
+        Integer passengerScore,
+        Integer driverScore,
+        ZonedDateTime bookRide,
+        ZonedDateTime cancelRide,
+        ZonedDateTime acceptRide,
+        ZonedDateTime beginRide,
+        ZonedDateTime endRide,
+        ZonedDateTime finishRide) {
+
+}
