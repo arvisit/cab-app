783c8551ca6b1aba8548f66840881f139a7d95c6
Artem Sitko <sitko.ar@gmail.com>
2024-04-17 14:26:12 +0300

    Notify passenger about accepted ride

---
+++ cab-app-passenger-service/src/main/java/by/arvisit/cabapp/passengerservice/messaging/MessagingConfiguration.java
@@ -0,0 +1,21 @@
+package by.arvisit.cabapp.passengerservice.messaging;
+
+import java.util.function.Consumer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import by.arvisit.cabapp.common.dto.rides.RideResponseDto;
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class MessagingConfiguration {
+
+    @Bean
+    Consumer<RideResponseDto> notifyAboutRideAcceptance() {
+        return ride -> {
+            log.info("Consumer received message about ride acceptance: {}", ride);
+        };
+    }
+}
