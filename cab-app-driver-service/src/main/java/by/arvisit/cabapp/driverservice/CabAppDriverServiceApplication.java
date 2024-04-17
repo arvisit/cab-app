555a390d894c223ebcedf9f83a65d475b68417c6
Artem Sitko <sitko.ar@gmail.com>
2024-04-17 15:15:34 +0300

    Enable feign client for driver service

--- cab-app-driver-service/src/main/java/by/arvisit/cabapp/driverservice/CabAppDriverServiceApplication.java
+++ cab-app-driver-service/src/main/java/by/arvisit/cabapp/driverservice/CabAppDriverServiceApplication.java
@@ -2,8 +2,10 @@ package by.arvisit.cabapp.driverservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CabAppDriverServiceApplication {
 
     public static void main(String[] args) {
