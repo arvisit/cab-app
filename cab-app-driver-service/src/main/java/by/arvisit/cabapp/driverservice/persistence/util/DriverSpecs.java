19fe4018ed19a45bfceea7010be75c9befc07d95
Artem Sitko <sitko.ar@gmail.com>
2024-04-24 22:00:23 +0300

    Reduce complexity of methods

--- cab-app-driver-service/src/main/java/by/arvisit/cabapp/driverservice/persistence/util/DriverSpecs.java
+++ cab-app-driver-service/src/main/java/by/arvisit/cabapp/driverservice/persistence/util/DriverSpecs.java
@@ -24,21 +24,27 @@ public class DriverSpecs {
         return (root, query, criteriaBuilder) -> {
             CriteriaBuilder cb = criteriaBuilder;
             Predicate spec = cb.conjunction();
+
+            if (filterParams == null) {
+                return spec;
+            }
+
             String carManufacturerNameValue = filterParams.get(CAR_MANUFACTURER_NAME_PARAM_NAME);
-            spec = cb.and(spec, getAllByCarManufacturerName(carManufacturerNameValue)
-                    .toPredicate(root, query, criteriaBuilder));
-            if (filterParams != null) {
-                for (Map.Entry<String, String> param : filterParams.entrySet()) {
-                    String paramKey = param.getKey();
-                    String paramValue = param.getValue();
-                    if (VALID_DIRECT_STRING_PARAM_NAMES.contains(paramKey) && !paramValue.trim().isEmpty()) {
-                        String likePattern = toLikePattern(paramValue);
-                        spec = cb.and(spec, cb.like(cb.lower(root.get(paramKey)), likePattern));
-                    }
-                    if (VALID_DIRECT_BOOLEAN_PARAM_NAMES.contains(paramKey) && !paramValue.trim().isEmpty()) {
-                        Boolean parsedValue = Boolean.valueOf(paramValue);
-                        spec = cb.and(spec, cb.equal(root.get(paramKey), parsedValue));
-                    }
+            if (carManufacturerNameValue != null && !carManufacturerNameValue.trim().isEmpty()) {
+                spec = cb.and(spec, getAllByCarManufacturerName(carManufacturerNameValue)
+                        .toPredicate(root, query, criteriaBuilder));
+            }
+
+            for (Map.Entry<String, String> param : filterParams.entrySet()) {
+                String paramKey = param.getKey();
+                String paramValue = param.getValue();
+                if (VALID_DIRECT_STRING_PARAM_NAMES.contains(paramKey) && !paramValue.trim().isEmpty()) {
+                    String likePattern = toLikePattern(paramValue);
+                    spec = cb.and(spec, cb.like(cb.lower(root.get(paramKey)), likePattern));
+                }
+                if (VALID_DIRECT_BOOLEAN_PARAM_NAMES.contains(paramKey) && !paramValue.trim().isEmpty()) {
+                    Boolean parsedValue = Boolean.valueOf(paramValue);
+                    spec = cb.and(spec, cb.equal(root.get(paramKey), parsedValue));
                 }
             }
             return spec;
@@ -48,14 +54,10 @@ public class DriverSpecs {
     private Specification<Driver> getAllByCarManufacturerName(String carManufacturerName) {
         return (root, query, criteriaBuilder) -> {
             CriteriaBuilder cb = criteriaBuilder;
-            Predicate spec = cb.conjunction();
-            if (carManufacturerName != null && !carManufacturerName.trim().isEmpty()) {
-                Join<Driver, Car> joinCar = root.join("car");
-                Join<Car, CarManufacturer> joinCarManufacturer = joinCar.join("manufacturer");
-                String likePattern = toLikePattern(carManufacturerName);
-                spec = cb.and(spec, cb.like(cb.lower(joinCarManufacturer.get("name")), likePattern));
-            }
-            return spec;
+            Join<Driver, Car> joinCar = root.join("car");
+            Join<Car, CarManufacturer> joinCarManufacturer = joinCar.join("manufacturer");
+            String likePattern = toLikePattern(carManufacturerName);
+            return cb.like(cb.lower(joinCarManufacturer.get("name")), likePattern);
         };
     }
 
