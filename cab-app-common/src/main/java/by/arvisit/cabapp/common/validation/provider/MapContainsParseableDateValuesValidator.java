21bf2b68b66bfd2791d65755e331335101704b4f
Artem Sitko <sitko.ar@gmail.com>
2024-04-25 15:36:37 +0300

    Add validation for parseability of strings into dates

---
+++ cab-app-common/src/main/java/by/arvisit/cabapp/common/validation/provider/MapContainsParseableDateValuesValidator.java
@@ -0,0 +1,40 @@
+package by.arvisit.cabapp.common.validation.provider;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import by.arvisit.cabapp.common.util.ValidationRegexp;
+import by.arvisit.cabapp.common.validation.MapContainsParseableDateValues;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class MapContainsParseableDateValuesValidator
+        implements ConstraintValidator<MapContainsParseableDateValues, Map<String, String>> {
+
+    private Set<String> targetKeys;
+
+    @Override
+    public void initialize(MapContainsParseableDateValues constraintAnnotation) {
+        targetKeys = Set.of(constraintAnnotation.keys());
+    }
+
+    @Override
+    public boolean isValid(Map<String, String> value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+        Pattern pattern = Pattern.compile(ValidationRegexp.DATE_AS_FILTER_PARAM_VALIDATION_REGEXP);
+        for (String key : targetKeys) {
+            if (value.containsKey(key)) {
+                Matcher matcher = pattern.matcher(value.get(key));
+                if (!matcher.matches()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
--- cab-app-rides-service/src/main/java/by/arvisit/cabapp/ridesservice/controller/RideController.java
+++ cab-app-rides-service/src/main/java/by/arvisit/cabapp/ridesservice/controller/RideController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import by.arvisit.cabapp.common.dto.ListContainerResponseDto;
 import by.arvisit.cabapp.common.validation.MapContainsAllowedKeys;
 import by.arvisit.cabapp.common.validation.MapContainsKey;
+import by.arvisit.cabapp.common.validation.MapContainsParseableDateValues;
 import by.arvisit.cabapp.common.validation.MapContainsParseableUUIDValues;
 import by.arvisit.cabapp.ridesservice.dto.RatingResponseDto;
 import by.arvisit.cabapp.ridesservice.dto.RideRequestDto;
@@ -222,7 +223,10 @@ public class RideController {
                             "finishRide" },
                     message = REQUEST_PARAMS_VALIDATION_NOT_ALLOWED_KEYS_MESSAGE)
             @MapContainsParseableUUIDValues(
-                    keys = { "passengerId", "driverId" }) Map<String, @NotBlank String> requestParams) {
+                    keys = { "passengerId", "driverId" })
+            @MapContainsParseableDateValues(
+                    keys = { "driverId", "bookRide", "cancelRide", "acceptRide", "beginRide", "endRide",
+                            "finishRide" }) Map<String, @NotBlank String> requestParams) {
         log.debug("Get all rides according to request parameters: {}", requestParams);
         ListContainerResponseDto<RideResponseDto> response = rideService.getRides(pageable, requestParams);
 
