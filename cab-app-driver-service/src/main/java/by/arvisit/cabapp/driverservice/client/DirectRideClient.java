fd04537ae7f8749c74599590b8cd67eadb1855da
Artem Sitko <sitko.ar@gmail.com>
2024-05-01 01:38:01 +0300

    Add profile for feign client to exclude from WebMvcTest

--- cab-app-driver-service/src/main/java/by/arvisit/cabapp/driverservice/client/DirectRideClient.java
+++ cab-app-driver-service/src/main/java/by/arvisit/cabapp/driverservice/client/DirectRideClient.java
@@ -3,6 +3,7 @@ package by.arvisit.cabapp.driverservice.client;
 import java.util.Map;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import by.arvisit.cabapp.common.dto.rides.RideResponseDto;
 
+@Profile({ "dev", "itest" })
 @FeignClient(name = "cab-app-rides-service", url = "${spring.settings.cab-app-rides-service.uri}",
         configuration = CabAppFeignClientConfiguration.class)
 public interface DirectRideClient extends RideClient {
