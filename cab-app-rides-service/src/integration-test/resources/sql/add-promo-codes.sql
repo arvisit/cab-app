ddfbf467a2f1ead56746204e51a411fe1d4af7ff
Artem Sitko <sitko.ar@gmail.com>
2024-05-05 17:07:09 +0300

    Add sql scripts for promo codes

---
+++ cab-app-rides-service/src/integration-test/resources/sql/add-promo-codes.sql
@@ -0,0 +1,10 @@
+INSERT INTO promo_codes(id, keyword, discount_percent, is_active) VALUES
+    (1, 'PAIN49', 49, TRUE),
+    (2, 'PAIN49', 49, FALSE),
+    (3, 'BRILLIANT10', 10, TRUE),
+    (4, 'RICE23', 23, FALSE);
+
+SELECT nextval('promo_codes_id_seq');
+SELECT nextval('promo_codes_id_seq');
+SELECT nextval('promo_codes_id_seq');
+SELECT nextval('promo_codes_id_seq');
