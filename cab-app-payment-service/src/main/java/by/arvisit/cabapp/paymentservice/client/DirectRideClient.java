9b5ca5d3608de0617995a697c31994ac4fd49822
Artem Sitko <sitko.ar@gmail.com>
2024-05-06 23:56:40 +0300

    Enable direct feign clients for dev, itest profiles

--- cab-app-payment-service/src/main/java/by/arvisit/cabapp/paymentservice/client/DirectRideClient.java
+++ cab-app-payment-service/src/main/java/by/arvisit/cabapp/paymentservice/client/DirectRideClient.java
@@ -1,11 +1,13 @@
 package by.arvisit.cabapp.paymentservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import by.arvisit.cabapp.common.dto.rides.RideResponseDto;
 
+@Profile({ "dev", "itest" })
 @FeignClient(name = "cab-app-rides-service", url = "${spring.settings.cab-app-rides-service.uri}",
         configuration = CabAppFeignClientConfiguration.class)
 public interface DirectRideClient extends RideClient {
