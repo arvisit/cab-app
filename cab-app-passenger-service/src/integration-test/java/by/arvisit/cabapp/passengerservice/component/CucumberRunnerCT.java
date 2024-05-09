82ab4e143ce580e373a12825346483d7dad34876
Artem Sitko <sitko.ar@gmail.com>
2024-05-09 14:56:52 +0300

    Set up Cucumber to run on JUnit5

--- cab-app-passenger-service/src/integration-test/java/by/arvisit/cabapp/passengerservice/component/CucumberRunnerCT.java
+++ cab-app-passenger-service/src/integration-test/java/by/arvisit/cabapp/passengerservice/component/CucumberRunnerCT.java
@@ -1,14 +1,18 @@
 package by.arvisit.cabapp.passengerservice.component;
 
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.core.options.Constants;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:features",
-        tags = "@component",
-        plugin = {"pretty"})
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("by/arvisit/cabapp/passengerservice/component")
+@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME, value = "classpath:features")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@component")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty")
 public class CucumberRunnerCT {
 
 }
