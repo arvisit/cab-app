8751160769738c32df846db1458541daf00add3d
Artem Sitko <sitko.ar@gmail.com>
2024-05-15 01:40:35 +0300

    Add profile for direct client

--- cab-app-driver-service/src/main/java/by/arvisit/cabapp/driverservice/client/DirectRideClient.java
+++ cab-app-driver-service/src/main/java/by/arvisit/cabapp/driverservice/client/DirectRideClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import by.arvisit.cabapp.common.dto.rides.RideResponseDto;
 
-@Profile({ "dev", "itest" })
+@Profile({ "dev", "itest", "e2e" })
 @FeignClient(name = "cab-app-rides-service", url = "${spring.settings.cab-app-rides-service.uri}",
         configuration = CabAppFeignClientConfiguration.class)
 public interface DirectRideClient extends RideClient {
