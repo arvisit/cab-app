93e04016a02b1c3c2b5a2879b6dbe1150a956742
Artem Sitko <sitko.ar@gmail.com>
2024-05-15 01:37:37 +0300

    Add sql scripts for draft passenger e2e tests

---
+++ script/e2e_sql/add_dbs.sql
@@ -0,0 +1,6 @@
+\connect postgres;
+
+CREATE DATABASE passenger_service_db;
+CREATE DATABASE driver_service_db;
+CREATE DATABASE rides_service_db;
+CREATE DATABASE payment_service_db;
---
+++ script/e2e_sql/fill_dbs.sql
@@ -0,0 +1,5 @@
+\include /home/e2e_sql/add_passengers.sql;
+\include /home/e2e_sql/add_cars_drivers.sql;
+\include /home/e2e_sql/add_promo_codes_rides.sql;
+\include /home/e2e_sql/add_passenger_payments.sql;
+\include /home/e2e_sql/add_driver_payments.sql;
