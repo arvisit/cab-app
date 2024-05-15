9ed9c5bc83afdb47634ae065e8bdfe2e30c0efeb
Artem Sitko <sitko.ar@gmail.com>
2024-05-16 01:14:06 +0300

    Remove excessive annotation

--- cab-app-rides-service/src/integration-test/java/by/arvisit/cabapp/ridesservice/component/CucumberRunnerComponentTest.java
+++ cab-app-rides-service/src/integration-test/java/by/arvisit/cabapp/ridesservice/component/CucumberRunnerComponentTest.java
@@ -2,14 +2,12 @@ package by.arvisit.cabapp.ridesservice.component;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import io.cucumber.core.options.Constants;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("by/arvisit/cabapp/ridesservice/component")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "by.arvisit.cabapp.ridesservice.component")
 @ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME, value = "classpath:features")
 @ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@component")
--- cab-app-rides-service/src/integration-test/java/by/arvisit/cabapp/ridesservice/e2e/CucumberRunnerE2E.java
+++ cab-app-rides-service/src/integration-test/java/by/arvisit/cabapp/ridesservice/e2e/CucumberRunnerE2E.java
@@ -2,14 +2,12 @@ package by.arvisit.cabapp.ridesservice.e2e;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import io.cucumber.core.options.Constants;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("by/arvisit/cabapp/ridesservice/e2e")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "by.arvisit.cabapp.ridesservice.e2e")
 @ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME, value = "classpath:features")
 @ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@e2e")
