6bedd3ce2ba815d942c46a5cebe22110f5d4775d
Artem Sitko <sitko.ar@gmail.com>
2024-05-18 18:15:55 +0300

    Add config server

---
+++ cab-app-config-server/src/main/java/by/arvisit/cabapp/configserver/CabAppConfigServerApplication.java
@@ -0,0 +1,17 @@
+package by.arvisit.cabapp.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class CabAppConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabAppConfigServerApplication.class, args);
+    }
+
+}
