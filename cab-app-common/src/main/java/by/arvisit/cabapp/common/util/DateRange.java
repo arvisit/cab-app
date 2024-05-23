226cdfa8e48f352289ad120a48af3218074abd99
Artem Sitko <sitko.ar@gmail.com>
2024-05-24 00:41:11 +0300

    Replace magic numbers with constants

--- cab-app-common/src/main/java/by/arvisit/cabapp/common/util/DateRange.java
+++ cab-app-common/src/main/java/by/arvisit/cabapp/common/util/DateRange.java
@@ -16,25 +16,30 @@ import lombok.Setter;
 @Builder(setterPrefix = "with")
 public class DateRange {
 
+    private static final int YEAR_MONTH_DAY_HOUR_STR_LENGTH = 13;
+    private static final int YEAR_MONTH_DAY_STR_LENGTH = 10;
+    private static final int YEAR_MONTH_STR_LENGTH = 7;
+    private static final int YEAR_STR_LENGTH = 4;
+
     private LocalDateTime startDate;
     private LocalDateTime endDate;
 
     public static DateRange fromSingleValue(String strDate) {
         LocalDateTime startDate = null;
         LocalDateTime endDate = null;
-        if (strDate.length() == 13) {
+        if (strDate.length() == YEAR_MONTH_DAY_HOUR_STR_LENGTH) {
             startDate = LocalDateTime.parse(strDate, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH"));
             endDate = startDate.plusHours(1).minusNanos(1);
         }
-        if (strDate.length() == 10) {
+        if (strDate.length() == YEAR_MONTH_DAY_STR_LENGTH) {
             startDate = LocalDate.parse(strDate, DateTimeFormatter.ofPattern("yyyy-MM-dd")).atStartOfDay();
             endDate = startDate.plusDays(1).minusNanos(1);
         }
-        if (strDate.length() == 7) {
+        if (strDate.length() == YEAR_MONTH_STR_LENGTH) {
             startDate = YearMonth.parse(strDate, DateTimeFormatter.ofPattern("yyyy-MM")).atDay(1).atStartOfDay();
             endDate = startDate.plusMonths(1).minusNanos(1);
         }
-        if (strDate.length() == 4) {
+        if (strDate.length() == YEAR_STR_LENGTH) {
             startDate = Year.parse(strDate).atMonth(Month.JANUARY).atDay(1).atStartOfDay();
             endDate = startDate.plusYears(1).minusNanos(1);
         }
