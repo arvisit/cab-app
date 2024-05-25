29a666ef72eed60b0941f2b59577bfdd887c5c16
Artem Sitko <sitko.ar@gmail.com>
2024-05-26 02:09:27 +0300

    Add common spec creation operations

--- cab-app-common/src/main/java/by/arvisit/cabapp/common/util/SpecUtil.java
+++ cab-app-common/src/main/java/by/arvisit/cabapp/common/util/SpecUtil.java
@@ -1,5 +1,12 @@
 package by.arvisit.cabapp.common.util;
 
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import jakarta.persistence.metamodel.SingularAttribute;
+
 public final class SpecUtil {
 
     private SpecUtil() {
@@ -9,13 +16,28 @@ public final class SpecUtil {
         return "%" + str.toLowerCase() + "%";
     }
 
-    public static Boolean fromString(String str) {
-        if ("true".compareToIgnoreCase(str) == 0) {
-            return true;
-        } else if ("false".compareToIgnoreCase(str) == 0) {
-            return false;
-        } else {
-            throw new IllegalArgumentException("Argument string could not be parsed as 'true' or 'false'");
-        }
+    public static <T> Specification<T> likeString(String str, SingularAttribute<T, String> attribute) {
+        return (root, query, cb) -> {
+            String likePattern = SpecUtil.toLikePattern(str);
+            return cb.like(cb.lower(root.get(attribute)), likePattern);
+        };
+    }
+
+    public static <T> Specification<T> equalBoolean(Boolean bool, SingularAttribute<T, Boolean> attribute) {
+        return (root, query, cb) -> cb.equal(root.get(attribute), bool);
+    }
+
+    public static <T> Specification<T> equalEnum(String str, SingularAttribute<T, ? extends Enum<?>> attribute) {
+        return (root, query, cb) -> cb.equal(root.get(attribute), str);
+    }
+
+    public static <T> Specification<T> equalUUID(UUID uuid, SingularAttribute<T, UUID> attribute) {
+        return (root, query, cb) -> cb.equal(root.get(attribute), uuid);
+    }
+
+    public static <T> Specification<T> betweenDateRange(DateRange dateRange,
+            SingularAttribute<T, ZonedDateTime> attribute) {
+        return (root, query, cb) -> cb.between(root.get(attribute), dateRange.getStartDate(),
+                dateRange.getEndDate());
     }
 }
