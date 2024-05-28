9e89a509131eec856911dea65b0c7653122efdd3
Artem Sitko <sitko.ar@gmail.com>
2024-05-28 12:50:38 +0300

    Revert "Disable messaging for integration tests"
    This reverts commit bce48bd2a7bd34ef1b3df2e8439cef4a5ef75453.

--- cab-app-passenger-service/src/main/java/by/arvisit/cabapp/passengerservice/messaging/MessagingConfiguration.java
+++ cab-app-passenger-service/src/main/java/by/arvisit/cabapp/passengerservice/messaging/MessagingConfiguration.java
@@ -4,12 +4,10 @@ import java.util.function.Consumer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import by.arvisit.cabapp.common.dto.rides.RideResponseDto;
 import lombok.extern.slf4j.Slf4j;
 
-@Profile("!itest")
 @Configuration
 @Slf4j
 public class MessagingConfiguration {
