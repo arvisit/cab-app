a0f364b29d1c78f53c352d0f1c2aeda2d06a4970
Artem Sitko <sitko.ar@gmail.com>
2024-05-29 15:01:55 +0300

    Correct variable names in the script

--- docker/kibana/init-index-patterns.sh
+++ docker/kibana/init-index-patterns.sh
@@ -7,19 +7,19 @@ until curl --output /dev/null --silent --head --fail $kibanaUrl; do
   sleep 5
 done
 
-for indx in *-index-pattern.json; do
-    indexTitle="$(echo "$indx" | cut -d'-' -f1)-*"
+for indexFile in *-index-pattern.json; do
+    indexTitle="$(echo "$indexFile" | cut -d'-' -f1)-*"
     response=$(curl -s -o /dev/null -w "%{http_code}" -X GET "$kibanaUrl/api/saved_objects/index-pattern/$indexTitle")
 
     # Create index pattern in Kibana
     if [ "$response" -eq 200 ]; then
-        echo "Index pattern $indx already exists."
+        echo "Index pattern $indexTitle already exists."
     else
-        curl -X POST "$kibanaUrl/api/saved_objects/index-pattern/$indx" \
+        curl -X POST "$kibanaUrl/api/saved_objects/index-pattern/$indexTitle" \
           -H 'kbn-xsrf: true' \
           -H 'Content-Type: application/json' \
-          -d @/usr/share/kibana/"$indx"
-        echo "Index pattern $indx created"
+          -d @/usr/share/kibana/"$indexFile"
+        echo "Index pattern $indexTitle created"
     fi
 done
 
