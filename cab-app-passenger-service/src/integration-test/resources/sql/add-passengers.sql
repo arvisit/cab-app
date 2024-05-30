f56bb4c743c9b784ba2a069e3b5243fa6a872fcc
arvisit <95239665+arvisit@users.noreply.github.com>
2024-05-30 10:34:38 +0300

    test/013 integration tests passenger service (#14)
    * Add dependencies for the integration tests
    
    * Add properties for tests
    
    * Add sql scripts for passengers table
    
    * Disable messaging for integration tests
    
    * Add testcontainer extension
    
    * Add test data for integration tests
    
    * Add integration tests
    
    * Revert "Disable messaging for integration tests"
    
    This reverts commit bce48bd2a7bd34ef1b3df2e8439cef4a5ef75453.
    
    * Enable Kafka for ITs
    
    * Remove excessive blank lines
    
    * Use IntegrationTest instead of IT postfix

---
+++ cab-app-passenger-service/src/integration-test/resources/sql/add-passengers.sql
@@ -0,0 +1,5 @@
+INSERT INTO passengers(id, name, email, card_number) VALUES
+    ('44012418-4211-4e65-aac6-c1f19f25715c', 'Johnny Doe', 'johnny.doe@yahoo.de', null),
+    ('51334f37-482c-4498-ad0f-122929edc0ff', 'John Doe', 'john.doe@mail.com', '8633928741544997'),
+    ('edeb6315-5408-46e6-a710-2666725c4dc7', 'Janny Doe', 'janny.doe@yahoo.com.br', null),
+    ('3abcc6a1-94da-4185-aaa1-8a11c1b8efd2', 'Jane Doe', 'jane.doe@yahoo.com.ar', '7853471929691513');
