d352f3f4599cad9a55d176f6e9043e2e8085680f
Artem Sitko <sitko.ar@gmail.com>
2024-06-06 10:13:55 +0300

    Replace kafka container stop with check if exists and not create another

--- cab-app-driver-service/src/integration-test/java/by/arvisit/cabapp/driverservice/KafkaTestContainerExtension.java
+++ cab-app-driver-service/src/integration-test/java/by/arvisit/cabapp/driverservice/KafkaTestContainerExtension.java
@@ -8,18 +8,21 @@ import org.testcontainers.utility.DockerImageName;
 
 public class KafkaTestContainerExtension implements BeforeAllCallback, AfterAllCallback {
 
+    private static final String SPRING_KAFKA_BOOTSTRAP_SERVERS = "spring.kafka.bootstrap-servers";
+
     static KafkaContainer kafkaContainer;
 
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
-        kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.4"));
-        kafkaContainer.start();
-        System.setProperty("spring.kafka.bootstrap-servers", kafkaContainer.getBootstrapServers());
+        if (System.getProperty(SPRING_KAFKA_BOOTSTRAP_SERVERS) == null) {
+            kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.4"));
+            kafkaContainer.start();
+            System.setProperty(SPRING_KAFKA_BOOTSTRAP_SERVERS, kafkaContainer.getBootstrapServers());
+        }
     }
 
     @Override
     public void afterAll(ExtensionContext context) throws Exception {
-        kafkaContainer.stop();
     }
 
 }
