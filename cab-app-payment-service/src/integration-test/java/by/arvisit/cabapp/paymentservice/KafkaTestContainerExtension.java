4fb86e538f49b245753224dbac95e5e11cd3af34
Artem Sitko <sitko.ar@gmail.com>
2024-06-11 10:58:29 +0300

    Add kafka container existence check

--- cab-app-payment-service/src/integration-test/java/by/arvisit/cabapp/paymentservice/KafkaTestContainerExtension.java
+++ cab-app-payment-service/src/integration-test/java/by/arvisit/cabapp/paymentservice/KafkaTestContainerExtension.java
@@ -1,6 +1,5 @@
 package by.arvisit.cabapp.paymentservice;
 
-
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -9,13 +8,17 @@ import org.testcontainers.utility.DockerImageName;
 
 public class KafkaTestContainerExtension implements BeforeAllCallback, AfterAllCallback {
 
+    private static final String SPRING_KAFKA_BOOTSTRAP_SERVERS = "spring.kafka.bootstrap-servers";
+
     static KafkaContainer kafkaContainer;
-    
+
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
-        kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.4"));
-        kafkaContainer.start();
-        System.setProperty("spring.kafka.bootstrap-servers", kafkaContainer.getBootstrapServers());
+        if (System.getProperty(SPRING_KAFKA_BOOTSTRAP_SERVERS) == null) {
+            kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.4"));
+            kafkaContainer.start();
+            System.setProperty(SPRING_KAFKA_BOOTSTRAP_SERVERS, kafkaContainer.getBootstrapServers());
+        }
     }
 
     @Override
