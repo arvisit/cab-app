315474eabc52cf900ee388cef96c4fb3968fa722
Artem Sitko <sitko.ar@gmail.com>
2024-06-18 11:28:41 +0300

    Correct getDriverAccountBalance request to handle no payments at all

--- cab-app-payment-service/src/main/java/by/arvisit/cabapp/paymentservice/persistence/repository/DriverPaymentRepository.java
+++ cab-app-payment-service/src/main/java/by/arvisit/cabapp/paymentservice/persistence/repository/DriverPaymentRepository.java
@@ -14,43 +14,45 @@ public interface DriverPaymentRepository extends JpaRepository<DriverPayment, UU
 
     @Query("""
             SELECT (
-                (SELECT SUM(
-                    CASE
-                        WHEN p.paymentMethod = by.arvisit.cabapp.paymentservice.persistence.model.PaymentMethodEnum.BANK_CARD
-                        THEN COALESCE(p.amount - p.feeAmount, 0)
-                        ELSE 0
-                    END
-                ) -
-                SUM(
-                    CASE
-                        WHEN p.paymentMethod = by.arvisit.cabapp.paymentservice.persistence.model.PaymentMethodEnum.CASH
-                        THEN COALESCE(p.feeAmount, 0)
-                        ELSE 0
-                    END
-                )
-                FROM PassengerPayment p
-                WHERE p.driverId = :id
-                    AND p.status = by.arvisit.cabapp.paymentservice.persistence.model.PaymentStatusEnum.SUCCESS
-                    ) +
-                (SELECT SUM(
-                    CASE
-                        WHEN d.operation = by.arvisit.cabapp.paymentservice.persistence.model.OperationTypeEnum.REPAYMENT
-                        THEN COALESCE(d.amount, 0)
-                        ELSE 0
-                    END
-                ) -
-                SUM(
-                    CASE
-                        WHEN d.operation = by.arvisit.cabapp.paymentservice.persistence.model.OperationTypeEnum.WITHDRAWAL
-                        THEN COALESCE(d.amount, 0)
-                        ELSE 0
-                    END
-                )
-                FROM DriverPayment d
-                WHERE d.driverId = :id
-                    AND d.status = by.arvisit.cabapp.paymentservice.persistence.model.PaymentStatusEnum.SUCCESS
+                (SELECT COALESCE(
+                    (SELECT SUM(
+                        CASE
+                            WHEN p.paymentMethod = by.arvisit.cabapp.paymentservice.persistence.model.PaymentMethodEnum.BANK_CARD
+                            THEN COALESCE(p.amount - p.feeAmount, 0)
+                            ELSE 0
+                        END
+                    ) -
+                    SUM(
+                        CASE
+                            WHEN p.paymentMethod = by.arvisit.cabapp.paymentservice.persistence.model.PaymentMethodEnum.CASH
+                            THEN COALESCE(p.feeAmount, 0)
+                            ELSE 0
+                        END
                     )
-                )
+                    FROM PassengerPayment p
+                    WHERE p.driverId = :id
+                        AND p.status = by.arvisit.cabapp.paymentservice.persistence.model.PaymentStatusEnum.SUCCESS
+                        ), 0)) +
+                (SELECT COALESCE(
+                    (SELECT SUM(
+                        CASE
+                            WHEN d.operation = by.arvisit.cabapp.paymentservice.persistence.model.OperationTypeEnum.REPAYMENT
+                            THEN COALESCE(d.amount, 0)
+                            ELSE 0
+                        END
+                    ) -
+                    SUM(
+                        CASE
+                            WHEN d.operation = by.arvisit.cabapp.paymentservice.persistence.model.OperationTypeEnum.WITHDRAWAL
+                            THEN COALESCE(d.amount, 0)
+                            ELSE 0
+                        END
+                    )
+                    FROM DriverPayment d
+                    WHERE d.driverId = :id
+                        AND d.status = by.arvisit.cabapp.paymentservice.persistence.model.PaymentStatusEnum.SUCCESS
+                        ), 0)
+                ))
             """)
     BigDecimal getDriverAccountBalance(@Param("id") UUID id);
 }
